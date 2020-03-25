0d1e931fc1cde3567795ac29163762bc1bf495a3
gwf123123 <939612831@qq.com>
2020-03-25 16:46:25 +0800

    新增文件

---
+++ First/src/com/imti/git/Git.java
@@ -0,0 +1,21 @@
+package com.imti.git;
+/**@文件名: Git.java
+ * @类功能说明: 
+ * @作者: GuanWenFei
+ * @Email: 939612831@qq.com
+ * @日期: 2020年3月25日下午4:29:01
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: GuanWenFei</li> 
+ * 	 <li>日期: 2020年3月25日下午4:29:01</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class Git {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
